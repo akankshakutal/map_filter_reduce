4f7df294629a4404d963622d03c50965fc4e9157
akankshka <44019583+akankshakutal@users.noreply.github.com>
2018-11-21 22:53:14 +0530

    Tests are changed
    Tests are changed according to functionality
    Test discription is changed

--- test/libTestUsingMocha.js
+++ test/libTestUsingMocha.js
@@ -33,55 +33,46 @@ const max = function(element1,element2) {
 
 /*------------------------- Tests ---------------*/
 describe ("map",function() {
-  it("should work for empty array",function(){
-    assert.deepEqual(lib.map(increment,[1]),[2]);
+  it("should return empty array for empty array",function(){
+    assert.deepEqual(lib.map(increment,[]),[]);
   });
-  it("should work for one element array",function(){
+  it("should return one element array for one element array",function(){
     assert.deepEqual(lib.map(square,[1]),[1]);
   });
-  it("should work for more than one element",function(){
+  it("should preserve the length of array of multiple elements",function(){
     assert.deepEqual(lib.map(square,[1,2,3]),[1,4,9]);
     assert.deepEqual(lib.map(cube,[1,2,3]),[1,8,27]);
-  });
-  it("should work for negative numbers",function(){
-    assert.deepEqual(lib.map(increment,[-4,-5,-6]),[-3,-4,-5]);
   });
 });
 
 
 describe ("filter",function() {
-  it("should work for empty array",function(){
+  it("should return empty array for empty array",function(){
     assert.deepEqual(lib.filter(isEven,[]),[]);
   });
-  it("should work for one element array",function(){
-    assert.deepEqual(lib.filter(square,[11]),[11]);
+  it("should return whole array for all truthy values",function(){
+    assert.deepEqual(lib.filter(isEven,[0,2,4,6]),[0,2,4,6]);
   });
-  it("should work for more than one element",function(){
-    assert.deepEqual(lib.filter(greaterThan,[1,2,30]),[30]);
-    assert.deepEqual(lib.filter(isEven,[1,2,4]),[2,4]);
+  it("should return empty array for all falsy values",function(){
+    assert.deepEqual(lib.filter(isEven,[1,3,5,7]),[]);
   });
-  it("should work for negative numbers",function(){
-    assert.deepEqual(lib.filter(greaterThan,[-10,-11,2,3,60]),[60]);
+  it("should return only truthy values from give array",function(){
+    assert.deepEqual(lib.filter(isEven,[1,2,3,4,5,6]),[2,4,6]);
   });
 });
 
 describe ("reduce",function() {
-  it("should work for empty array",function(){
+  it("should return undefined for empty array without accumulator",function(){
     assert.deepEqual(lib.reduce(sumOfAllElements,[]),undefined);
-    assert.deepEqual(lib.reduce(max,[]),undefined);
   });
-  it("should work for one element array",function(){
-    assert.deepEqual(lib.reduce(max,[11]),11);
+  it("should return accumulator value for empty array with accumulator value",function(){
+    assert.deepEqual(lib.reduce(max,[],2),2);
   });
-  it("should work for more than one element",function(){
+  it("should return one value for array of multiple element",function(){
     assert.deepEqual(lib.reduce(sumOfAllElements,[1,2,30]),33);
     assert.deepEqual(lib.reduce(max,[4,2,1],2),4);
   });
-  it("should work for negative numbers",function(){
-    assert.deepEqual(lib.reduce(max,[-10,-11,2,3,60]),60);
-  });
-  it("should work for undefined ",function(){
+  it("should return undefined for array element as undefined ",function(){
     assert.deepEqual(lib.reduce(max,[undefined]),undefined);
-    assert.deepEqual(lib.reduce(sumOfAllElements,[undefined]),undefined);
   });
 });
